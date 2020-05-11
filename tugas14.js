04ae542135a99cd32808248b4693f28c3da7da05
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:13:18 +0700

    Tugas 14 JS
    -Buatlah sebuah variabel untuk melakukan operasi pembagian , teman teman lakukan pembagian 37 dibagi tanggal lahir teman teman.
    -Setelah itu teman teman tampilkan hasil pembagian tersebut.
    -Setelah itu teman teman tampilkan kembali hasilnya dengan perintah math round , ceil dan floor.

---
+++ tugas14.js
@@ -0,0 +1,6 @@
+x=37
+tanggalLahir=2
+var hasilBagi = x / tanggalLahir  //18.5
+console.log(Math.round(hasilBagi)) //depend >= .5 atau < .5
+console.log(Math.ceil(hasilBagi)) //dibulatkan ke atas
+console.log(Math.floor(hasilBagi)) //dibulatkan ke bawah
